b321b104a7383ecfd1c430232a85e5dc009c8f10
eudald-guillen-clarrio <eudald.guillen@klarrio.com>
2025-09-30 17:34:02 +0200

    fix gitsigns

--- nvim/lua/eudaldgm/lazy/gitsigns.lua
+++ nvim/lua/eudaldgm/lazy/gitsigns.lua
@@ -1,9 +1,20 @@
-return{
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require('gitsigns').setup({
-		})
-	end,
-	vim.keymap.set('n', '<leader>Gb', ':Gitsigns blame<CR>', {silent = true, desc = "Open gitsigns gitblame"}),
+return {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
 
+    gitsigns.setup({
+			current_line_blame_opts = {
+				delay = 10,
+			}
+		})
+
+    -- Keymap to show full blame in a floating window
+    vim.keymap.set('n', '<leader>Gl', function()
+      gitsigns.toggle_current_line_blame()
+    end, { desc = "Toggle inline gitblame" })
+
+    -- Keymap to trigger Gitsigns blame command
+    vim.keymap.set('n', '<leader>Gb', ':Gitsigns blame<CR>', { silent = true, desc = "Open Gitsigns gitblame" })
+  end
 }
