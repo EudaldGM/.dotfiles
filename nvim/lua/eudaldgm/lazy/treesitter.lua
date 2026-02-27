a2d434eb2b2f78d80119e6055b67c28bf31f5857
eudald-guillen-clarrio <eudald.guillen@klarrio.com>
2026-02-27 01:05:21 +0100

    fixes

--- nvim/lua/eudaldgm/lazy/treesitter.lua
+++ nvim/lua/eudaldgm/lazy/treesitter.lua
@@ -1,54 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile"},
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   config = function()
-    -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
+    treesitter.setup({
       highlight = {
         enable = true,
       },
-      -- enable indentation
       indent = { enable = true },
-      -- ensure these language parsers are installed
       ensure_installed = {
         "json",
         "yaml",
         "html",
         "css",
         "bash",
-		"helm",
         "lua",
         "vim",
         "dockerfile",
         "gitignore",
         "vimdoc",
-		"python",
-		"toml",
-		"go",
-		"rust",
-		"hcl",
-		"terraform",
-		"zig"
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
+        "python",
+        "toml",
+        "go",
+        "rust",
+        "hcl",
+        "terraform",
+        "zig",
       },
     })
-
-    -- use bash parser for zsh files
     vim.treesitter.language.register("bash", "zsh")
   end,
 }
